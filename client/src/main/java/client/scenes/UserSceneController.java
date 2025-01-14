d3fcc1ba6fbf764845ed23e502aa6176e18ea58b
Maksymilian Maciejek <M.I.Maciejek@student.tudelft.nl>
2025-01-14 22:39:41 +0100

    Add color picker and handle checkBox click

--- client/src/main/java/client/scenes/UserSceneController.java
+++ client/src/main/java/client/scenes/UserSceneController.java
@@ -2,10 +2,7 @@ package client.scenes;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
 public class UserSceneController {
@@ -25,6 +22,9 @@ public class UserSceneController {
     @FXML
     private CheckBox newAccountCheckBox;
 
+    @FXML
+    private ColorPicker colorPicker;
+
     public void initialize() {
         System.out.println("Welcome to UserSceneController");
         loginButton.setOnAction(event -> {handleLoginClick();});
@@ -32,9 +32,28 @@ public class UserSceneController {
 
     @FXML
     public void handleLoginClick() {
-        System.out.println("Login clicked");
+        boolean check = newAccountCheckBox.isSelected();
         String username = this.usernameField.getText();
         String password = this.passwordField.getText();
+
+        if(check) {
+
+        }else{
+
+        }
+
         System.out.println("username: " + username + " password: " + password);
+    }
+
+    @FXML
+    public void handleCheckBoxClick(){
+        boolean check = newAccountCheckBox.isSelected();
+        if(check){
+            this.loginButton.setText("Register");
+            colorPicker.setVisible(true);
+        }else{
+            this.loginButton.setText("Login");
+            colorPicker.setVisible(false);
+        }
     }
 }
